85ddb30155147a23744b1ca75619266f87799986
hyuntae99 <hyuntae9912@naver.com>
2023-03-26 02:25:01 +0000

    Automated commit Sun Mar 26 02:25:01 UTC 2023

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Auto Commit Program
 Sun Mar 26 02:22:31 UTC 2023
+Sun Mar 26 02:25:01 UTC 2023
---
+++ makeDummy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+filename=$(date '+%Y-%m-%d_%H-%M-%S').txt
+touch /home/hyuntae9912/autoCommit/ACP/dummy/$filename
